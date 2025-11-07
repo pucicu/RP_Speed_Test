6d77f3f742cd011c60a4defb3d89e20afed4e7c3
Norbert Marwan <marwan@pik-potsdam.de>
2025-11-07 19:05:06 +0100

    add accRQA by Adamek

--- run_software_test.sh
+++ run_software_test.sh
@@ -1,6 +1,13 @@
+module load python/3.12.3
+module load cuda/12.6.0 
+module load julia/1.11.7
+
+python3.12 software_speed_accrqa.py --compFlag=cpu
+python3.12 software_speed_accrqa.py --compFlag=nv_gpu
+
 python3 software_speed.py
 python3 software_speed_pyunicorn.py
 julia software_speed.jl
 julia --threads 16 software_speed_parallel.jl
 #rscript software_speed.R
-matlab -nodesktop < software_speed.m
+#matlab -nodesktop < software_speed.m
---
+++ software_speed_accrqa.py
@@ -0,0 +1,68 @@
+# speed test
+
+# import required packages
+from scipy.integrate import odeint
+import numpy as np
+import time
+import accrqa as rqa
+import argparse
+
+# argumentParser
+parser = argparse.ArgumentParser(description="Set computation flag")
+
+# optional argument --compFlag, default 'nv_gpu'
+parser.add_argument(
+    '--compFlag',
+    type=str,
+    choices=['nv_gpu', 'cpu'],
+    default='nv_gpu',
+    help="Computation flag: 'nv_gpu' or 'cpu' (default: 'nv_gpu')"
+)
+
+# parse arguments
+args = parser.parse_args()
+
+# set computation type
+compFlag = args.compFlag
+
+
+# the Roessler ODE
+def roessler(x,t):
+   return [-(x[1] + x[2]), x[0] + 0.25 * x[1], 0.25 + (x[0] - 4) * x[2]]
+
+# solve the ODE
+x = odeint(roessler, [0, 0, 0], np.arange(0, 5500, .05))
+
+# length of time series for RQA calculation test
+N = np.round(10**np.arange(2.3,5.6,.075)). astype(int)
+
+
+# calculate RP and RQA for different length
+tspanRP = np.zeros(len(N)); # result vector computation time
+tspanRQA = np.zeros(len(N)); # result vector computation time
+K = 10; # number of runs (for averaging time)
+maxT = 60; # stop calculations if maxT is exceeded
+
+for i in range(0,len(tspanRP)):
+    tRP_ = 0
+    tRQA_ = 0
+    for j in range(0,K):
+        start_time = time.time()
+        
+        output_RR = rqa.RR(x[1000:(1000+N[i]),0], np.array([6], dtype=np.intc), np.array([3], dtype=np.intc), np.array([1.2]), distance_type=rqa.accrqaDistance("euclidean"), comp_platform = rqa.accrqaCompPlatform(compFlag), tidy_data = False);
+        output_DET = rqa.DET(x[1000:(1000+N[i]),0], np.array([6], dtype=np.intc), np.array([3], dtype=np.intc), np.array([2], dtype=np.intc), np.array([1.2]), distance_type=rqa.accrqaDistance("euclidean"), calculate_ENTR = True, comp_platform = rqa.accrqaCompPlatform(compFlag), tidy_data = False);
+        output_LAM = rqa.LAM(x[1000:(1000+N[i]),0], np.array([6], dtype=np.intc), np.array([3], dtype=np.intc), np.array([2], dtype=np.intc), np.array([1.2]), distance_type=rqa.accrqaDistance("euclidean"), calculate_ENTR = True, comp_platform = rqa.accrqaCompPlatform(compFlag), tidy_data = False);
+
+        #tRP_ += (time.time() - start_time)
+        tRQA_ += (time.time() - start_time)
+    tspanRP[i] = tRP_ / K # average calculation time
+    tspanRQA[i] = tRQA_ / K # average calculation time
+    print(N[i], ": ", tspanRP[i], " ", tspanRQA[i])
+    
+    if tspanRP[i] + tspanRQA[i] >= maxT:
+       break
+
+tspanRP
+
+np.savetxt(f'time_python_accrqa_{compFlag}.csv',list(zip(N, tspanRQA)))
+
