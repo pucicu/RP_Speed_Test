bf22be5861ea34d6ebdcb16a705a790f03bb0b54
Norbert Marwan <marwan@pik-potsdam.de>
2025-11-25 09:43:50 +0100

    update MATLAB version

--- Scripts/software_speed.m
+++ Scripts/software_speed.m
@@ -20,23 +20,15 @@ options = odeset('RelTol',1e-8,'AbsTol',1e-10);
 t(1:1000) = []; x(1:1000,:) = [];
 
 %% length of time series for RQA calculation test
-N = round(10.^(2:.075:5.06));
+N = round(10.^(log10(200):.075:log10(200000)));
+N = round(10.^(log10(200):.075:log10(10000)));
 
 
 %% calculate RP and RQA for different length
 tspanRP = zeros(length(N), 1); % result vector computation time
 tspanRQA = zeros(length(N), 1); % result vector computation time
 K = 10; % number of runs (for averaging time)
-maxT = 60; % stop calculations if maxT is exceeded
-
-% % using CRP toolbox (slow because of GUI framework)
-% for i = 1:length(N)
-%     tic
-%     %R = crp(x(1:1+N(i)-1,3), 3, 6, 1.2, 'euc', 'nonorm','silent'); 
-%     Q = crqa(x(1:1+N(i)-1,3), 3, 6, 1.2, [], 'euc', 'nonorm','silent'); 
-%     tspan(i) = toc;
-% end
-% tspan
+maxT = 600; % stop calculations if maxT is exceeded
 
 for i = 1:length(N)
     tRP_ = 0;
@@ -50,20 +42,17 @@ for i = 1:length(N)
         tic
         Q = rqa(R, 2, 1, 'non');
         tRQA_ = tRQA_ + toc;
-        
-        disp(sprintf('  %i', j))
     end
     tspanRP(i) = tRP_ / K; % average calculation time
     tspanRQA(i) = tRQA_ / K; % average calculation time
     disp(sprintf('%i: %f %f', N(i), tspanRP(i), tspanRQA(i)))
     
     % save results
-    ex = [N(:) tspanRP(:) tspanRQA(:)];
+    ex = [N(:) tspanRP(:) tspanRQA(:) tspanRP(:)+tspanRQA(:)];
     save(filename,'ex','-ascii','-tabs')
 
     if tspanRP(i) + tspanRQA(i) >= maxT, break, end
     
 end
-N(1:4) = []; tspanRP(1:4) = []; tspanRQA(1:4) = [];% remove first points before N=200
 
 exit
