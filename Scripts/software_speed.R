a758965170e06c063b6ff89becee1ddb05b38cf2
Norbert Marwan <marwan@pik-potsdam.de>
2026-03-01 19:09:05 +0100

    add RP calculation time in R script

--- Scripts/software_speed.R
+++ Scripts/software_speed.R
@@ -20,7 +20,8 @@ x = as.matrix(read.table(datafile))
 N = round(10.^seq(log10(200),log10(100000), 0.075))
 
 # calculate RP and RQA for different length
-tspan = numeric(length(N))  # result vector computation time
+tspanRQA = numeric(length(N))  # result vector computation time
+tspanRP = numeric(length(N))  # result vector computation time
 mRQA = matrix(0, nrow = length(N), ncol = 6) # result vector RQA average
 vRQA = matrix(0, nrow = length(N), ncol = 6) # result vector RQA variance
 K = 10                      # number of runs (for averaging time)
@@ -38,6 +39,34 @@ for (i in 1:length(N)) {
 
    for (j in 1:K) {
 
+ 
+        start_time = proc.time()[3]
+
+        R = try(
+          crqa(
+            x[1:N[i]],
+            x[1:N[i]],
+            tau, m, 0, e, 0, lmin, lmin, 1,
+            FALSE, FALSE, "both", "rqa", "euclidean", "continuous"
+          ),
+          silent = TRUE
+        )
+
+        if (inherits(R, "try-error")) {
+
+          cat("Error in crqa at i =", i, "j =", j, "- skip calculation\n")
+          t_ = NaN
+
+        } else {
+
+          X  = R$RP
+
+          end_time = proc.time()[3]
+          duration = end_time - start_time
+
+          tRP_ = t_ + duration
+        }
+ 
         start_time = proc.time()[3]
 
         R = try(
@@ -67,19 +96,20 @@ for (i in 1:length(N)) {
           end_time = proc.time()[3]
           duration = end_time - start_time
 
-          t_ = t_ + duration
+          tRQA_ = t_ + duration
           RQA_[j, ] = c(rr, det, l, ent, lam, tt)
         }
 
        #cat("  ", j, "\n")
     }
-    tspan[i] = t_ / K # average calculation time
+    tspanRP[i] = tRP_ / K # average calculation time
+    tspanRQA[i] = tRQA_ / K # average calculation time
     mRQA[i, ] = colMeans(RQA_)
     vRQA[i, ] = apply(RQA_, 2, var)
-    cat(N[i], ": ", tspan[i], "\n")
+    cat(N[i], ": ", tspanRQA[i], "\n")
         
     # save results
-    writeLines(sprintf("%d, %f, %f", N[i], NaN, tspan[i]), f_time)
+    writeLines(sprintf("%d, %f, %f", N[i], tspanRP[i], tspanRQA[i]), f_time)
     line <- paste(
       N[i],
       paste(mRQA[i, ], collapse = ", "),
@@ -88,7 +118,7 @@ for (i in 1:length(N)) {
     )
     writeLines(line, f_rqa)
 
-    if (tspan[i] >= maxT) {
+    if (tspanRQA[i] >= maxT) {
        break
     }
 }    
